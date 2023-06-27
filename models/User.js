f4672fa40220c70a012e37e881347a3ce1fc17f4
Nick JDA <armstrongnick1999@gmail.com>
2023-06-27 19:03:41 -0400

    Created User model with virtuals

--- models/User.js
+++ models/User.js
@@ -1,11 +1,48 @@
 const { Schema, model } = require('mongoose');
 
 
-const userSchema = new Schema(
+const userSchema = new Schema(      
     {
-
+        userName: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [
+                /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,6}$/,
+                "Email not Valid"
+            ],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
     }
 );
+
+userSchema
+    .virtual('firendscount').get(function () {
+        return this.friends.length;
+    });
 
 const User = model('user', userSchema);
 
