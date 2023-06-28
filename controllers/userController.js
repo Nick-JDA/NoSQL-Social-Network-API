ad0edb027d736d1e365446ba5589a5df05dc5cf5
Nick JDA <armstrongnick1999@gmail.com>
2023-06-28 09:58:19 -0400

    added gitignore file, created first function (getUsers) for userControllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,40 @@
-const { User, Thought } = require('../models');
+const { User, Thought } = require("../models");
 
 module.exports = {
-    // get all users
+  // get all users
+  async getUsers(req, res) {
+    try {
+      const users = await User.findAll();
 
-    // get a single user by id and populated thought and friend data
+      const userObj = {
+        users,
+      };
 
-    // post create a new user
+      res.json(userObj);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
 
-    // put to update a user by its id
+  // get a single user by id and populated thought and friend data
+  async getSingleUser(req, res) {},
 
-    // delete to remove user by its id
+  // post create a new user
+  async createUser(req, res) {},
 
+  // put to update a user by its id
+  async updateUser(req, res) {},
 
-    //-------------------------------------
-    // api/users/:userId/friends/:friendId
+  // delete to remove user by its id
+  async deleteUser(req, res) {},
 
-    // post to add a new friend to a users friend list
+  //-------------------------------------
+  // api/users/:userId/friends/:friendId
 
-    // delete to remove a friend from a user's friend list
+  // post to add a new friend to a users friend list
+  async addFriend(req, res) {},
+
+  // delete to remove a friend from a user's friend list
+  async deleteFriend(req, res) {},
 };
