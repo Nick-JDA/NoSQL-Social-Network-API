d1401b9902cf3bd7667980aacda3d372d068a9d1
Nick JDA <armstrongnick1999@gmail.com>
2023-06-27 21:29:06 -0400

    created thought and user controllers with layout for where functions will go, started userRoutes no functionality yet

---
+++ controllers/thoughtController.js
@@ -0,0 +1,22 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // get to get all thoughts
+
+    // get to get a single thought by its id
+
+    // post to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+
+    // put to update a thought by its id
+
+    // delete to remove a thought by its id
+
+
+
+    //-------------------------------------
+    // api/thoughts/:thoughtId/reactions
+
+    // post to create a reaction stored in a single thought's reactions array field
+
+    // delete to pull and remove a reaction by the reaction's reactionId value
+};
---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // get all users
+
+    // get a single user by id and populated thought and friend data
+
+    // post create a new user
+
+    // put to update a user by its id
+
+    // delete to remove user by its id
+
+
+    //-------------------------------------
+    // api/users/:userId/friends/:friendId
+
+    // post to add a new friend to a users friend list
+
+    // delete to remove a friend from a user's friend list
+};
